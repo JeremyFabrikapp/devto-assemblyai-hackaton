5d5d7de8ccb621052b6772f7c96b2ec27713d282
Jeremy <jeremy@fabrikapp.fr>
2024-11-30 00:25:57 +0100

    static

--- frontend/app/notes/[id]/page.tsx
+++ frontend/app/notes/[id]/page.tsx
@@ -4,11 +4,11 @@ import NoteDetail from '@/components/notes/NoteDetail';
 export function generateStaticParams() {
   // In a real app, this would come from your data source
   return [
-    { id: '1' },
-    { id: '2' },
-    { id: '3' },
-    { id: '4' },
-    { id: '5' }
+    // { id: '1' },
+    // { id: '2' },
+    // { id: '3' },
+    // { id: '4' },
+    // { id: '5' }
   ];
 }
 
--- frontend/app/sessions/[id]/page.tsx
+++ frontend/app/sessions/[id]/page.tsx
@@ -5,8 +5,8 @@ import { getSessionById } from "@/app/actions/database";
 // Generate static params for build time
 export function generateStaticParams() {
   return [
-    { id: '074565d3-37b7-4389-85c0-d3388482242b' },
-    { id: 'f79d4829-0394-4bc1-9de8-2b15850f392a' },
+    // { id: '074565d3-37b7-4389-85c0-d3388482242b' },
+    // { id: 'f79d4829-0394-4bc1-9de8-2b15850f392a' },
     // { id: '3' },
     // { id: '4' },
     // { id: '5' }
